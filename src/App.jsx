0356c1c5141452d68eb2d925573b9212872c58bf
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-04-05 11:58:54 +0200

    J'ai ajouter un useState pour recuperer la valeur de input et l'ajouter dans le tableau

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,38 @@ import fstyle from './app.module.css'
 function App() {
 
   const [task,setTask] = useState([]);
-
+  // Pour gerer la v aleur de l'input 
+  const [inputValue , setInputValue] = useState('');
+ 
   const funcAddTask = (e) =>{
-    console.log("test");
+    // console.log("test");
+    e.preventDefault();
+    let values = e.target.value;
+    // console.log(values);
+    setTask([...task,values]);
+
+    
+     console.log(task);
+
+    
   }
+
+  
   
   return (
     <>
       <div className={fstyle.main}>
         <Input />
         <Button text="Creer une tache " color="#008000" onClick={funcAddTask} />
+      </div>
+
+      <div>
+        <h2>Tâches :</h2>
+        <ul>
+          {task.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
       </div>
         
     
