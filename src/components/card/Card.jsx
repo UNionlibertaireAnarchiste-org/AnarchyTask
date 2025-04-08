7726bdbe8d054906f5d08ab52360242cb0527760
Tatiana-Juin <tatiana.juinp15@gmail.com>
2025-04-08 19:47:13 +0200

    L'affichage par rapport au condition fonctionne

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,25 +6,45 @@ import { useState } from 'react'
 export default function Card( {valueTask} ){
 
     const [valueModif,setValueModif] = useState(false);
+    const [task,setTask] = useState([]);
+    const [inputValue , setInputValue] = useState('');
 
     const funcInput = (e) =>{
         e.preventDefault();
         // Pour alterner l'affichage de input 
         setValueModif(!valueModif);
+        console.log(valueTask)
+    }
+
+    const handleChange = (e) =>{
+        setInputValue(e.target.value);
+    }
+
+    const funcAddTask = ()=>{
+        
     }
 
     return(
         <>
             <div key={valueTask} className={fstyle.card}>
-                
-                {/* Affiche dans le nom de la tache quand il n'y a pas de champs input  */}
-                {!valueModif && <p>{valueTask}</p> }
-                
-                {valueModif && <Input valueTask={valueTask}/>}
 
-                <Button text="Modifier " color="#008000" onClick={funcInput} />
-                {/* Quand il n'y a pas l'input modifier */}
-                {!valueModif &&  <Button text="Supprimer " color="red" /> }
+                {/* Quand il n'y a pas de champs input  */}
+                {valueModif ? (
+                    <>
+                       <Input valueTask={valueTask} onChange={handleChange}/> 
+                       <Button text="Modifier " color="#008000" onClick={funcAddTask} />
+                    </>
+                ): (
+                    <>
+                      <p> {valueTask} </p>
+                        <Button text="Modifier " color="#008000" onClick={funcInput} />
+                        <Button text="Supprimer " color="red" />
+                     
+                    </>
+                  )
+                }
+
+             
                 
                 
             </div>
